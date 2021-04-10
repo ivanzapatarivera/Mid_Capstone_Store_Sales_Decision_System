c8e8ced437a66377799b66f599de490172ac0051
Ivan Zapata <falenyo@gmail.com>
2021-04-09 23:23:32 -0500

    Models OK

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Importing required dependencies
+from config import username, password, server, database, db_uri
+import config
 import os
 from flask import Flask, redirect, render_template, url_for
 from flask_migrate import Migrate
@@ -11,8 +13,6 @@ from sqlalchemy.dialects.mysql import *
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'mysecretkey'
 
-import config
-from config import username, password, server, database, db_uri
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,13 +23,115 @@ db = SQLAlchemy(app)
 Migrate(app, db)
 
 
+class Employees(db.Model):
+
+    __tablename__ = 'employees'
+
+    emp_name = Column(VARCHAR(64), nullable=False)
+    pay_grade = Column(VARCHAR(3), nullable=False)
+    region = Column(VARCHAR(2), nullable=False)
+    emp_id = Column(VARCHAR(7), nullable=False, primary_key=True)
+
+    def __init__(self, emp_name, pay_grade, region, emp_id):
+        self.emp_name = emp_name
+        self.pay_grade = pay_grade
+        self.region = region
+        self.emp_id = emp_id
+
+    def __repr__(self):
+        return f'{self.emp_name} \n {self.pay_grade} \n {self.region} \n {self.emp_id} \n'
+
+
+class ItemsOffered(db.Model):
+
+    __tablename__ = 'items_offered'
+
+    item_code = Column(VARCHAR(8), nullable=False, primary_key=True)
+    item_name = Column(VARCHAR(40), nullable=False)
+    url = Column(Text, nullable=True)
+    link = Column(Text, nullable=True)
+    manufacturer = Column(VARCHAR(40), nullable=True)
+
+    def __init__(self, item_code, item_name, url, link, manufacturer):
+        self.item_code = item_code
+        self.item_name = item_name
+        self.url = url
+        self.link = link
+        self.manufacturer = manufacturer
+
+    def __repr__(self):
+        return f'{self.item_code} \n {self.item_name} \n {self.url} \n {self.link} \n {self.manufacturer}'
+
+
 class ProductSales(db.Model):
 
     __tablename__ = 'product_sales'
 
-    sale_id = Column(Integer(display_width = 10), nullable = False)
-    index = Column(Integer(display_width = 10), nullable = False)
-    item_code = 
+    index = Column(INTEGER(display_width=10), nullable=False)
+    item_code = Column(VARCHAR(10), ForeignKey(
+        ItemsOffered.item_code, onupdate='CASCADE'), nullable=False)
+    emp_id = Column(VARCHAR(6), ForeignKey(
+        Employees.emp_id, onupdate='CASCADE'), nullable=False)
+    attribute = Column(VARCHAR(2), nullable=False, primary_key=True)
+    year = Column(SMALLINT(4), nullable=False, primary_key=True)
+    value = Column(INTEGER(display_width=10), nullable=False)
+
+    def __init__(self, index, item_code, emp_id, attribute, year, value):
+        self.index = index
+        self.item_code = item_code
+        self.emp_id = emp_id
+        self.attribute = attribute
+        self.year = year
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.index} \n{self.item_code} \n{self.emp_id} \n{self.attribute} \n {self.year} \n{self.value}"
+
+
+class SalesPeriods(db.Model):
+
+    __tablename__ = 'sales_periods'
+
+    date = Column(DATE, nullable=False, primary_key=True)
+    attribute = Column(VARCHAR(3), nullable=False)
+    sales_period = Column(TINYINT(2), nullable=False)
+    sales_year = Column(SMALLINT(4), nullable=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            [attribute, sales_year],
+            [ProductSales.attribute, ProductSales.year],
+        ),
+    )
+
+    def __init__(self, date, attribute, sales_period, sales_year):
+        self.date = date
+        self.attribute = attribute
+        self.sales_period = sales_period
+        self.sales_year = sales_year
+
+    def __repr__(self):
+        return f'{self.date} {self.attribute} {self.sales_period} {self.sales_year}'
+
+
+class ProductPriceChange(db.Model):
+
+    __tablename__ = 'product_price_change'
+
+    price_id = Column(INTEGER, primary_key=True, autoincrement=True)
+    item_code = Column(VARCHAR(10), ForeignKey(
+        ItemsOffered.item_code, onupdate='CASCADE'), nullable=False)
+    attribute = Column(VARCHAR(6), nullable=False)
+    value = Column(INTEGER(display_width=10), nullable=False)
+
+    def __init__(self, price_id, item_code, attribute, value):
+        self.price_id = price_id
+        self.item_code = item_code
+        self.attribute = attribute
+        self.value = value
+
+    def __repr__(self):
+        return f'{self.price_id} {self.item_code} {self.attribute} {self.value}'
 
 
 db.create_all()
